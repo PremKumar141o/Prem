ae35092df916358e7c6691594095cdb3ec97d47e
PremKumar141o <145246629+PremKumar141o@users.noreply.github.com>
2023-09-20 19:35:13 +0530

    Update Withdraw.py

--- Withdraw.py
+++ Withdraw.py
@@ -1,8 +1,31 @@
-def isLeap(n):
-    if((n%4==0 and n%100!=0) or n%400==0): return 1
-    else: return 0
-
-if(isLeap(int(input("Enter a Year : ")))): print("It is a Leap Year")
-else: print("It is not a Leap Year ")
-
+class Bank_Account:
+    __balance=0.0
+    __acntno=0
+    __acnthldnm=""
+    def __init__(self):
+        print("Hello!\nWelcome to the Deposit & Withdrawal Machine")
+        self.__acntno=int(input("\nEnter ur Account No: "))
+        self.__acnthldnm=input("Enter ur Name: ")
+ 
+    def deposit(self):
+        amount=float(input("\nEnter amount to be Deposited: "))
+        self.__balance += amount
+        print("Amount Deposited:",amount)
+ 
+    def withdraw(self):
+        amount = float(input("\nEnter Amount to be Withdrawn: "))
+        print("\nWithdral Report:")
+        if self.__balance>=amount:
+            self.__balance-=amount
+            print("Account No: ",self.__acntno,"\nAccount Holder Name: ",self.__acnthldnm,"\n Your Withdrew: ", amount)
+        else:
+            print("\n Insufficient __balance  ")
+ 
+    def display(self):
+        print("\nBalance Report:","\nAccount No: ",self.__acntno,"\nAccount Holder Name: ",self.__acnthldnm,"\nNet Available Balance=",self.__balance)
+ 
+s = Bank_Account()
+s.deposit()
+s.withdraw()
+s.display()
 input()
